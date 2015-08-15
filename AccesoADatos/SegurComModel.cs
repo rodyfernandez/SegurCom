0fdfd64ad1d2949080e0a5254ae4477971dab48e
Rodolfo Fernandez <rodyfernandez87@gmail.com>
2015-08-15 14:47:47 -0300

    Iniciacion de proyecto SegurCom

---
+++ AccesoADatos/SegurComModel.cs
@@ -0,0 +1,32 @@
+namespace AccesoADatos
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+    
+
+    public class SegurComModel : DbContext
+    {
+        // Your context has been configured to use a 'SegurComModel' connection string from your application's 
+        // configuration file (App.config or Web.config). By default, this connection string targets the 
+        // 'AccesoADatos.SegurComModel' database on your LocalDb instance. 
+        // 
+        // If you wish to target a different database and/or database provider, modify the 'SegurComModel' 
+        // connection string in the application configuration file.
+        public SegurComModel()
+            : base("name=SegurComModel")
+        {
+        }
+
+        // Add a DbSet for each entity type that you want to include in your model. For more information 
+        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
